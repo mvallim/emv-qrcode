71f64ef0f563e4fbdbe3b2494e662ed10b838579
Marcos Tischer Vallim <tischer@gmail.com>
2020-08-07 14:29:40 -0300

    Add Isos

---
+++ src/main/java/com/emv/qrcode/core/iso/Country.java
@@ -0,0 +1,115 @@
+package com.emv.qrcode.core.iso;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import lombok.Getter;
+
+/**
+ *
+ * ISO 3166 country codes
+ *
+ */
+@Getter
+public enum Country {
+
+  AAX("X", "ALA", "248"), AL("AL", "ALB", "008"), DZ("DZ", "DZA", "012"), AS("AS", "ASM", "016"),
+  AD("AD", "AND", "020"), AO("AO", "AGO", "024"), AI("AI", "AIA", "660"), AQ("AQ", "ATA", "010"),
+  AG("AG", "ATG", "028"), AR("AR", "ARG", "032"), AM("AM", "ARM", "051"), AW("AW", "ABW", "533"),
+  AU("AU", "AUS", "036"), AT("AT", "AUT", "040"), AZ("AZ", "AZE", "031"), BS("BS", "BHS", "044"),
+  BH("BH", "BHR", "048"), BD("BD", "BGD", "050"), BB("BB", "BRB", "052"), BY("BY", "BLR", "112"),
+  BE("BE", "BEL", "056"), BZ("BZ", "BLZ", "084"), BJ("BJ", "BEN", "204"), BM("BM", "BMU", "060"),
+  BT("BT", "BTN", "064"), BO("BO", "BOL", "068"), BQ("BQ", "BES", "535"), BA("BA", "BIH", "070"),
+  BW("BW", "BWA", "072"), BV("BV", "BVT", "074"), BR("BR", "BRA", "076"), IO("IO", "IOT", "086"),
+  BN("BN", "BRN", "096"), BG("BG", "BGR", "100"), BF("BF", "BFA", "854"), BI("BI", "BDI", "108"),
+  CV("CV", "CPV", "132"), KH("KH", "KHM", "116"), CM("CM", "CMR", "120"), CA("CA", "CAN", "124"),
+  KY("KY", "CYM", "136"), CF("CF", "CAF", "140"), TD("TD", "TCD", "148"), CL("CL", "CHL", "152"),
+  CN("CN", "CHN", "156"), CX("CX", "CXR", "162"), CC("CC", "CCK", "166"), CO("CO", "COL", "170"),
+  KM("KM", "COM", "174"), CD("CD", "COD", "180"), CG("CG", "COG", "178"), CK("CK", "COK", "184"),
+  CR("CR", "CRI", "188"), CI("CI", "CIV", "384"), HR("HR", "HRV", "191"), CU("CU", "CUB", "192"),
+  CW("CW", "CUW", "531"), CY("CY", "CYP", "196"), CZ("CZ", "CZE", "203"), DK("DK", "DNK", "208"),
+  DJ("DJ", "DJI", "262"), DM("DM", "DMA", "212"), DO("DO", "DOM", "214"), EC("EC", "ECU", "218"),
+  EG("EG", "EGY", "818"), SV("SV", "SLV", "222"), GQ("GQ", "GNQ", "226"), ER("ER", "ERI", "232"),
+  EE("EE", "EST", "233"), SZ("SZ", "SWZ", "748"), ET("ET", "ETH", "231"), FK("FK", "FLK", "238"),
+  FO("FO", "FRO", "234"), FJ("FJ", "FJI", "242"), FI("FI", "FIN", "246"), FR("FR", "FRA", "250"),
+  GF("GF", "GUF", "254"), PF("PF", "PYF", "258"), TF("TF", "ATF", "260"), GA("GA", "GAB", "266"),
+  GM("GM", "GMB", "270"), GE("GE", "GEO", "268"), DE("DE", "DEU", "276"), GH("GH", "GHA", "288"),
+  GI("GI", "GIB", "292"), GR("GR", "GRC", "300"), GL("GL", "GRL", "304"), GD("GD", "GRD", "308"),
+  GP("GP", "GLP", "312"), GU("GU", "GUM", "316"), GT("GT", "GTM", "320"), GG("GG", "GGY", "831"),
+  GN("GN", "GIN", "324"), GW("GW", "GNB", "624"), GY("GY", "GUY", "328"), HT("HT", "HTI", "332"),
+  HM("HM", "HMD", "334"), VA("VA", "VAT", "336"), HN("HN", "HND", "340"), HK("HK", "HKG", "344"),
+  HU("HU", "HUN", "348"), IS("IS", "ISL", "352"), IN("IN", "IND", "356"), ID("ID", "IDN", "360"),
+  IR("IR", "IRN", "364"), IQ("IQ", "IRQ", "368"), IE("IE", "IRL", "372"), IM("IM", "IMN", "833"),
+  IL("IL", "ISR", "376"), IT("IT", "ITA", "380"), JM("JM", "JAM", "388"), JP("JP", "JPN", "392"),
+  JE("JE", "JEY", "832"), JO("JO", "JOR", "400"), KZ("KZ", "KAZ", "398"), KE("KE", "KEN", "404"),
+  KI("KI", "KIR", "296"), KP("KP", "PRK", "408"), KR("KR", "KOR", "410"), KW("KW", "KWT", "414"),
+  KG("KG", "KGZ", "417"), LA("LA", "LAO", "418"), LV("LV", "LVA", "428"), LB("LB", "LBN", "422"),
+  LS("LS", "LSO", "426"), LR("LR", "LBR", "430"), LY("LY", "LBY", "434"), LI("LI", "LIE", "438"),
+  LT("LT", "LTU", "440"), LU("LU", "LUX", "442"), MO("MO", "MAC", "446"), MK("MK", "MKD", "807"),
+  MG("MG", "MDG", "450"), MW("MW", "MWI", "454"), MY("MY", "MYS", "458"), MV("MV", "MDV", "462"),
+  ML("ML", "MLI", "466"), MT("MT", "MLT", "470"), MH("MH", "MHL", "584"), MQ("MQ", "MTQ", "474"),
+  MR("MR", "MRT", "478"), MU("MU", "MUS", "480"), YT("YT", "MYT", "175"), MX("MX", "MEX", "484"),
+  FM("FM", "FSM", "583"), MD("MD", "MDA", "498"), MC("MC", "MCO", "492"), MN("MN", "MNG", "496"),
+  ME("ME", "MNE", "499"), MS("MS", "MSR", "500"), MA("MA", "MAR", "504"), MZ("MZ", "MOZ", "508"),
+  MM("MM", "MMR", "104"), NA("NA", "NAM", "516"), NR("NR", "NRU", "520"), NP("NP", "NPL", "524"),
+  NL("NL", "NLD", "528"), NC("NC", "NCL", "540"), NZ("NZ", "NZL", "554"), NI("NI", "NIC", "558"),
+  NE("NE", "NER", "562"), NG("NG", "NGA", "566"), NU("NU", "NIU", "570"), NF("NF", "NFK", "574"),
+  MP("MP", "MNP", "580"), NO("NO", "NOR", "578"), OM("OM", "OMN", "512"), PK("PK", "PAK", "586"),
+  PW("PW", "PLW", "585"), PS("PS", "PSE", "275"), PA("PA", "PAN", "591"), PG("PG", "PNG", "598"),
+  PY("PY", "PRY", "600"), PE("PE", "PER", "604"), PH("PH", "PHL", "608"), PN("PN", "PCN", "612"),
+  PL("PL", "POL", "616"), PT("PT", "PRT", "620"), PR("PR", "PRI", "630"), QA("QA", "QAT", "634"),
+  RE("RE", "REU", "638"), RO("RO", "ROU", "642"), RU("RU", "RUS", "643"), RW("RW", "RWA", "646"),
+  BL("BL", "BLM", "652"), KN("KN", "KNA", "659"), LC("LC", "LCA", "662"), MF("MF", "MAF", "663"),
+  PM("PM", "SPM", "666"), VC("VC", "VCT", "670"), SH("SH", "SHN", "654"), WS("WS", "WSM", "882"),
+  SM("SM", "SMR", "674"), ST("ST", "STP", "678"), SA("SA", "SAU", "682"), SN("SN", "SEN", "686"),
+  RS("RS", "SRB", "688"), SC("SC", "SYC", "690"), SL("SL", "SLE", "694"), SG("SG", "SGP", "702"),
+  SX("SX", "SXM", "534"), SK("SK", "SVK", "703"), SI("SI", "SVN", "705"), SB("SB", "SLB", "090"),
+  SO("SO", "SOM", "706"), ZA("ZA", "ZAF", "710"), GS("GS", "SGS", "239"), SS("SS", "SSD", "728"),
+  ES("ES", "ESP", "724"), LK("LK", "LKA", "144"), SD("SD", "SDN", "729"), SR("SR", "SUR", "740"),
+  SJ("SJ", "SJM", "744"), SE("SE", "SWE", "752"), CH("CH", "CHE", "756"), SY("SY", "SYR", "760"),
+  TW("TW", "TWN", "158"), TJ("TJ", "TJK", "762"), TZ("TZ", "TZA", "834"), TH("TH", "THA", "764"),
+  TL("TL", "TLS", "626"), TG("TG", "TGO", "768"), TK("TK", "TKL", "772"), TO("TO", "TON", "776"),
+  TT("TT", "TTO", "780"), TN("TN", "TUN", "788"), TR("TR", "TUR", "792"), TM("TM", "TKM", "795"),
+  TC("TC", "TCA", "796"), TV("TV", "TUV", "798"), UG("UG", "UGA", "800"), UA("UA", "UKR", "804"),
+  AE("AE", "ARE", "784"), GB("GB", "GBR", "826"), UM("UM", "UMI", "581"), US("US", "USA", "840"),
+  UY("UY", "URY", "858"), UZ("UZ", "UZB", "860"), VU("VU", "VUT", "548"), VE("VE", "VEN", "862"),
+  VN("VN", "VNM", "704"), VG("VG", "VGB", "092"), VI("VI", "VIR", "850"), WF("WF", "WLF", "876"),
+  EH("EH", "ESH", "732"), YE("YE", "YEM", "887"), ZM("ZM", "ZMB", "894"), ZW("ZW", "ZWE", "716");
+
+  private static final Map<String, Country> mapString = new HashMap<>();
+
+  static {
+    for (final Country country : EnumSet.allOf(Country.class)) {
+      mapString.put(country.getCode(), country);
+    }
+  }
+
+  private final String alpha2;
+
+  private final String alpha3;
+
+  private final String code;
+
+  private Country(final String alpha2, final String alpha3, final String code) {
+    this.alpha2 = alpha2;
+    this.alpha3 = alpha3;
+    this.code = code;
+  }
+
+  @Override
+  public String toString() {
+    return code;
+  }
+
+  public static Country entryOf(final String code) {
+    return StringUtils.isNoneBlank(code) ? mapString.get(code) : null;
+  }
+
+  public static boolean exists(final String lang) {
+    return StringUtils.isNoneBlank(lang) && mapString.containsKey(lang);
+  }
+
+}
---
+++ src/main/java/com/emv/qrcode/core/iso/Currency.java
@@ -0,0 +1,79 @@
+package com.emv.qrcode.core.iso;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+import lombok.Getter;
+
+/**
+ *
+ * ISO 4217 currency codes
+ *
+ */
+@Getter
+public enum Currency {
+
+  AED("AED", "784"), AFN("AFN", "971"), ALL("ALL", "008"), AMD("AMD", "051"), ANG("ANG", "532"), AOA("AOA", "973"),
+  ARS("ARS", "032"), AUD("AUD", "036"), AWG("AWG", "533"), AZN("AZN", "944"), BAM("BAM", "977"), BBD("BBD", "052"),
+  BDT("BDT", "050"), BGN("BGN", "975"), BHD("BHD", "048"), BIF("BIF", "108"), BMD("BMD", "060"), BND("BND", "096"),
+  BOB("BOB", "068"), BOV("BOV", "984"), BRL("BRL", "986"), BSD("BSD", "044"), BTN("BTN", "064"), BWP("BWP", "072"),
+  BYN("BYN", "933"), BZD("BZD", "084"), CAD("CAD", "124"), CDF("CDF", "976"), CHE("CHE", "947"), CHF("CHF", "756"),
+  CHW("CHW", "948"), CLF("CLF", "990"), CLP("CLP", "152"), CNY("CNY", "156"), COP("COP", "170"), COU("COU", "970"),
+  CRC("CRC", "188"), CUC("CUC", "931"), CUP("CUP", "192"), CVE("CVE", "132"), CZK("CZK", "203"), DJF("DJF", "262"),
+  DKK("DKK", "208"), DOP("DOP", "214"), DZD("DZD", "012"), EGP("EGP", "818"), ERN("ERN", "232"), ETB("ETB", "230"),
+  EUR("EUR", "978"), FJD("FJD", "242"), FKP("FKP", "238"), GBP("GBP", "826"), GEL("GEL", "981"), GHS("GHS", "936"),
+  GIP("GIP", "292"), GMD("GMD", "270"), GNF("GNF", "324"), GTQ("GTQ", "320"), GYD("GYD", "328"), HKD("HKD", "344"),
+  HNL("HNL", "340"), HRK("HRK", "191"), HTG("HTG", "332"), HUF("HUF", "348"), IDR("IDR", "360"), ILS("ILS", "376"),
+  INR("INR", "356"), IQD("IQD", "368"), IRR("IRR", "364"), ISK("ISK", "352"), JMD("JMD", "388"), JOD("JOD", "400"),
+  JPY("JPY", "392"), KES("KES", "404"), KGS("KGS", "417"), KHR("KHR", "116"), KMF("KMF", "174"), KPW("KPW", "408"),
+  KRW("KRW", "410"), KWD("KWD", "414"), KYD("KYD", "136"), KZT("KZT", "398"), LAK("LAK", "418"), LBP("LBP", "422"),
+  LKR("LKR", "144"), LRD("LRD", "430"), LSL("LSL", "426"), LYD("LYD", "434"), MAD("MAD", "504"), MDL("MDL", "498"),
+  MGA("MGA", "969"), MKD("MKD", "807"), MMK("MMK", "104"), MNT("MNT", "496"), MOP("MOP", "446"), MRU("MRU", "929"),
+  MUR("MUR", "480"), MVR("MVR", "462"), MWK("MWK", "454"), MXN("MXN", "484"), MXV("MXV", "979"), MYR("MYR", "458"),
+  MZN("MZN", "943"), NAD("NAD", "516"), NGN("NGN", "566"), NIO("NIO", "558"), NOK("NOK", "578"), NPR("NPR", "524"),
+  NZD("NZD", "554"), OMR("OMR", "512"), PAB("PAB", "590"), PEN("PEN", "604"), PGK("PGK", "598"), PHP("PHP", "608"),
+  PKR("PKR", "586"), PLN("PLN", "985"), PYG("PYG", "600"), QAR("QAR", "634"), RON("RON", "946"), RSD("RSD", "941"),
+  RUB("RUB", "643"), RWF("RWF", "646"), SAR("SAR", "682"), SBD("SBD", "090"), SCR("SCR", "690"), SDG("SDG", "938"),
+  SEK("SEK", "752"), SGD("SGD", "702"), SHP("SHP", "654"), SLL("SLL", "694"), SOS("SOS", "706"), SRD("SRD", "968"),
+  SSP("SSP", "728"), STN("STN", "930"), SVC("SVC", "222"), SYP("SYP", "760"), SZL("SZL", "748"), THB("THB", "764"),
+  TJS("TJS", "972"), TMT("TMT", "934"), TND("TND", "788"), TOP("TOP", "776"), TRY("TRY", "949"), TTD("TTD", "780"),
+  TWD("TWD", "901"), TZS("TZS", "834"), UAH("UAH", "980"), UGX("UGX", "800"), USD("USD", "840"), USN("USN", "997"),
+  UYI("UYI", "940"), UYU("UYU", "858"), UYW("UYW", "927"), UZS("UZS", "860"), VES("VES", "928"), VND("VND", "704"),
+  VUV("VUV", "548"), WST("WST", "882"), XAF("XAF", "950"), XAG("XAG", "961"), XAU("XAU", "959"), XBA("XBA", "955"),
+  XBB("XBB", "956"), XBC("XBC", "957"), XBD("XBD", "958"), XCD("XCD", "951"), XDR("XDR", "960"), XOF("XOF", "952"),
+  XPD("XPD", "964"), XPF("XPF", "953"), XPT("XPT", "962"), XSU("XSU", "994"), XTS("XTS", "963"), XUA("XUA", "965"),
+  XXX("XXX", "999"), YER("YER", "886"), ZAR("ZAR", "710"), ZMW("ZMW", "967"), ZWL("ZWL", "932");
+
+  private static final Map<String, Currency> mapString = new HashMap<>();
+
+  static {
+    for (final Currency currency : EnumSet.allOf(Currency.class)) {
+      mapString.put(currency.getCode(), currency);
+    }
+  }
+
+  private final String code;
+  private final String number;
+
+  private Currency(final String code, final String number) {
+    this.code = code;
+    this.number = number;
+  }
+
+  @Override
+  public String toString() {
+    return code;
+  }
+
+  public static Currency entryOf(final String code) {
+    return StringUtils.isNoneBlank(code) ? mapString.get(code) : null;
+  }
+
+  public static boolean exists(final String code) {
+    return StringUtils.isNoneBlank(code) && mapString.containsKey(code);
+  }
+
+}
