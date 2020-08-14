7f9c234306355e9d609d88383091d25373c50bb5
Marcos Tischer Vallim <tischer@gmail.com>
2020-08-14 15:51:53 -0300

    Improve tests

--- src/test/java/com/emv/qrcode/core/CRCTest.java
+++ src/test/java/com/emv/qrcode/core/CRCTest.java
@@ -11,7 +11,9 @@ public class CRCTest {
   public void testSuccessCrc16() {
     final String value = "00020101021229300012D156000000000510A93FO3230Q31280012D15600000001030812345678520441115802CN5914BEST TRANSPORT6007BEIJING64200002ZH0104最佳运输0202北京540523.7253031565502016233030412340603***0708A60086670902ME91320016A0112233449988770708123456786304";
 
-    assertThat(Integer.toHexString(CRC.crc16(value.getBytes())).toUpperCase(), equalTo("A13A"));
+    final int crc16 = CRC.crc16(value.getBytes());
+
+    assertThat(String.format("%04X", crc16), equalTo("A13A"));
   }
 
 }
