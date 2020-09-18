d4d196c4c003cf55aaf9efcde7d43d3eec92a3d6
Marcos Tischer Vallim <tischer@gmail.com>
2020-09-18 11:45:05 -0300

    Add CRC16 code on decoder

--- src/main/java/com/emv/qrcode/decoder/mpm/MerchantPresentedModeDecoder.java
+++ src/main/java/com/emv/qrcode/decoder/mpm/MerchantPresentedModeDecoder.java
@@ -37,6 +37,7 @@ public final class MerchantPresentedModeDecoder extends DecoderMpm<MerchantPrese
     mapConsumers.put(MerchantPresentedModeCodes.ID_MERCHANT_ACCOUNT_INFORMATION, consumerTagLengthValue(MerchantAccountInformationTemplate.class, MerchantPresentedMode::addMerchantAccountInformation));
     mapConsumers.put(MerchantPresentedModeCodes.ID_RFU_FOR_EMVCO, consumerTagLengthValue(TagLengthString.class, MerchantPresentedMode::addRFUforEMVCo));
     mapConsumers.put(MerchantPresentedModeCodes.ID_UNRESERVED_TEMPLATES, consumerTagLengthValue(UnreservedTemplate.class, MerchantPresentedMode::addUnreserved));
+    mapConsumers.put(MerchantPresentedModeCodes.ID_CRC, consumerTagLengthValue(String.class, MerchantPresentedMode::setCRC));
   }
 
   public MerchantPresentedModeDecoder(final String source) {
