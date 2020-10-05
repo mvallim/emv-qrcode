3b28f245bd579473b05ccb423194c75cb9cdc67c
Rafael Volpato <rafael@x4fare.com>
2020-10-05 18:39:25 -0300

    removendo codigos específicios

--- src/main/java/br/gov/bcb/brcode/BRCodeConstants.java
+++
@@ -1,8 +0,0 @@
-package br.gov.bcb.brcode;
-
-public class BRCodeConstants {
-
-    public static final String PIX_GUI = "br.gov.bcb.pix";
-
-    public static final String BRCODE_GUI = "br.gov.bcb.brcode";
-}
