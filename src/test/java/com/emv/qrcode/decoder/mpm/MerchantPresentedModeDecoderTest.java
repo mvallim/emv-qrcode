2a206d5a413892d944569246b76b677e553cc277
Marcos Tischer Vallim <tischer@gmail.com>
2021-03-13 02:03:43 -0300

    Changes:
    * Improve tests

--- src/test/java/com/emv/qrcode/decoder/mpm/MerchantPresentedModeDecoderTest.java
+++ src/test/java/com/emv/qrcode/decoder/mpm/MerchantPresentedModeDecoderTest.java
@@ -1,12 +1,15 @@
 package com.emv.qrcode.decoder.mpm;
 
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 
 import org.junit.Test;
 
+import com.emv.qrcode.core.exception.DuplicateTagException;
 import com.emv.qrcode.core.exception.MerchantPresentedModeException;
 import com.emv.qrcode.model.mpm.MerchantPresentedMode;
 
@@ -234,6 +237,20 @@ public class MerchantPresentedModeDecoderTest {
     assertThat(merchantPresentMode.toString().length(), equalTo(301));
     assertThat(merchantPresentMode.toString(), equalTo(
         "00020101021102160004hoge0104abcd520441115303156540523.725502015603500570155802CN5914BEST TRANSPORT6007BEIJING6107123456762950105123450205678900305098760405543210505abcde0605fghij0705klmno0805pqres0905tuvxy5010000110101i64280002ZH0102北京0204最佳运输0304abcd65020080320016A01122334499887707081234567863044220"));
+  }
+
+  @Test
+  public void testeFasilDuplicateTag() throws MerchantPresentedModeException {
+    final String encoded = "00020101021102160004hoge0104abcd5204411153031565303156540523.725502015603500570155802CN5914BEST TRANSPORT6007BEIJING6107123456762950105123450205678900305098760405543210505abcde0605fghij0705klmno0805pqres0905tuvxy5010000110101i64280002ZH0102北京0204最佳运输0304abcd65020080320016A011223344998877070812345678";
+
+    final MerchantPresentedModeException merchantPresentedModeException = catchThrowableOfType(() -> DecoderMpm.decode(encoded, MerchantPresentedMode.class), MerchantPresentedModeException.class);
+
+    assertThat(merchantPresentedModeException, instanceOf(DuplicateTagException.class));
+
+    final DuplicateTagException duplicateTagException = DuplicateTagException.class.cast(merchantPresentedModeException);
+
+    assertThat(duplicateTagException.getTag(), equalTo("53"));
+    assertThat(duplicateTagException.getValue(), equalTo("5303156"));
   }
 
 }
