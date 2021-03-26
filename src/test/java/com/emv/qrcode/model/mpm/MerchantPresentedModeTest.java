d2643617f298aecb81a33b671d538686ac08a295
Marcos Tischer Vallim <tischer@gmail.com>
2021-03-25 23:41:23 -0300

    Changes:
    * Update README.md

--- src/test/java/com/emv/qrcode/model/mpm/MerchantPresentedModeTest.java
+++ src/test/java/com/emv/qrcode/model/mpm/MerchantPresentedModeTest.java
@@ -15,13 +15,15 @@ import com.emv.qrcode.core.exception.PresentedModeException;
 import com.emv.qrcode.core.model.mpm.TagLengthString;
 import com.emv.qrcode.decoder.mpm.DecoderMpm;
 
+// @formatter:off
 public class MerchantPresentedModeTest {
 
   @Test
   public void testSuccessToString() {
 
     final AdditionalDataFieldTemplate additionalDataField = getAddtionalDataField();
-    final MerchantAccountInformationTemplate merchantAccountInformation = getMerchanAccountInformation();
+    final MerchantAccountInformationTemplate merchanAccountInformationReservedAdditional = getMerchanAccountInformationReservedAdditional();
+    final MerchantAccountInformationTemplate merchanAccountInformationReserved = getMerchanAccountInformationReserved();
     final MerchantInformationLanguageTemplate merchantInformationLanguage = getMerchantInformationLanguage();
     final UnreservedTemplate unreserved = getUnreserved();
     final TagLengthString rFUforEMVCo = new TagLengthString("65", "00");
@@ -41,16 +43,29 @@ public class MerchantPresentedModeTest {
     merchantPresentMode.setTransactionCurrency("156");
     merchantPresentMode.setValueOfConvenienceFeeFixed("500");
     merchantPresentMode.setValueOfConvenienceFeePercentage("5");
-    merchantPresentMode.addMerchantAccountInformation(merchantAccountInformation);
+    merchantPresentMode.addMerchantAccountInformation(merchanAccountInformationReserved);
+    merchantPresentMode.addMerchantAccountInformation(merchanAccountInformationReservedAdditional);
     merchantPresentMode.addRFUforEMVCo(rFUforEMVCo);
     merchantPresentMode.addUnreserved(unreserved);
 
-    assertThat(merchantPresentMode.toString(), equalTo(
-        "00020101021102160004hoge0104abcd520441115303156540523.725502015603500570155802CN5914BEST TRANSPORT6007BEIJING6107123456762950105123450205678900305098760405543210505abcde0605fghij0705klmno0805pqres0905tuvxy5010000110101i64280002ZH0102北京0204最佳运输0304abcd65020080320016A01122334499887707081234567863044220"));
+    assertThat(merchantPresentMode.toString(), equalTo("0002010102110204000426160004"
+        + "hoge0104abcd520441115303156540523.725502015603500570155802CN5914BEST TRAN"
+        + "SPORT6007BEIJING6107123456762950105123450205678900305098760405543210505ab"
+        + "cde0605fghij0705klmno0805pqres0905tuvxy5010000110101i64280002ZH0102北京020"
+        + "4最佳运输0304abcd65020080320016A011223344998877070812345678630432B3"));
 
   }
 
-  private MerchantAccountInformationTemplate getMerchanAccountInformation() {
+  // Primitive Payment System Merchant Account Information (IDs "02" to "25")
+  private MerchantAccountInformationTemplate getMerchanAccountInformationReserved() {
+
+    final MerchantAccountInformationReserved merchantAccountInformationValue = new MerchantAccountInformationReserved("0004");
+
+    return new MerchantAccountInformationTemplate("02", merchantAccountInformationValue);
+  }
+
+  // Merchant Account Information Template (IDs "26" to "51")
+  private MerchantAccountInformationTemplate getMerchanAccountInformationReservedAdditional() {
 
     final TagLengthString paymentNetworkSpecific = new TagLengthString();
     paymentNetworkSpecific.setTag("01");
@@ -60,10 +75,7 @@ public class MerchantPresentedModeTest {
     merchantAccountInformationValue.setGloballyUniqueIdentifier("hoge");
     merchantAccountInformationValue.addPaymentNetworkSpecific(paymentNetworkSpecific);
 
-    final MerchantAccountInformationTemplate merchantAccountInformation = new MerchantAccountInformationTemplate();
-    merchantAccountInformation.setValue(merchantAccountInformationValue);
-    merchantAccountInformation.setTag("02");
-    return merchantAccountInformation;
+    return new MerchantAccountInformationTemplate("26", merchantAccountInformationValue);
   }
 
   private UnreservedTemplate getUnreserved() {
@@ -178,12 +190,22 @@ public class MerchantPresentedModeTest {
 
   @Test
   public void testSuccessToBase64() throws PresentedModeException {
-    final String encoded = "00020101021102160004hoge0104abcd520441115303156540523.725502015603500570155802CN5914BEST TRANSPORT6007BEIJING6107123456762950105123450205678900305098760405543210505abcde0605fghij0705klmno0805pqres0905tuvxy5010000110101i64280002ZH0102北京0204最佳运输0304abcd65020080320016A01122334499887707081234567863044220";
+    final String encoded = "00020101021102160004hoge0104abcd5204411153031565405"
+        + "23.725502015603500570155802CN5914BEST TRANSPORT6007BEIJING6107123456"
+        + "762950105123450205678900305098760405543210505abcde0605fghij0705klmno"
+        + "0805pqres0905tuvxy5010000110101i64280002ZH0102北京0204最佳运输0304abcd"
+        + "65020080320016A01122334499887707081234567863044220";
 
     final MerchantPresentedMode merchantPresentMode = DecoderMpm.decode(encoded, MerchantPresentedMode.class);
 
-    assertThat(merchantPresentMode.toBase64(), equalTo(
-        "MDAwMjAxMDEwMjExMDIxNjAwMDRob2dlMDEwNGFiY2Q1MjA0NDExMTUzMDMxNTY1NDA1MjMuNzI1NTAyMDE1NjAzNTAwNTcwMTU1ODAyQ041OTE0QkVTVCBUUkFOU1BPUlQ2MDA3QkVJSklORzYxMDcxMjM0NTY3NjI5NTAxMDUxMjM0NTAyMDU2Nzg5MDAzMDUwOTg3NjA0MDU1NDMyMTA1MDVhYmNkZTA2MDVmZ2hpajA3MDVrbG1ubzA4MDVwcXJlczA5MDV0dXZ4eTUwMTAwMDAxMTAxMDFpNjQyODAwMDJaSDAxMDLljJfkuqwwMjA05pyA5L2z6L+Q6L6TMDMwNGFiY2Q2NTAyMDA4MDMyMDAxNkEwMTEyMjMzNDQ5OTg4NzcwNzA4MTIzNDU2Nzg2MzA0NDIyMA=="));
+    assertThat(merchantPresentMode.toBase64(), equalTo("MDAwMjAxMDEwMjExMDIxNjA"
+        + "wMDRob2dlMDEwNGFiY2Q1MjA0NDExMTUzMDMxNTY1NDA1MjMuNzI1NTAyMDE1NjAzNTA"
+        + "wNTcwMTU1ODAyQ041OTE0QkVTVCBUUkFOU1BPUlQ2MDA3QkVJSklORzYxMDcxMjM0NTY"
+        + "3NjI5NTAxMDUxMjM0NTAyMDU2Nzg5MDAzMDUwOTg3NjA0MDU1NDMyMTA1MDVhYmNkZTA"
+        + "2MDVmZ2hpajA3MDVrbG1ubzA4MDVwcXJlczA5MDV0dXZ4eTUwMTAwMDAxMTAxMDFpNjQ"
+        + "yODAwMDJaSDAxMDLljJfkuqwwMjA05pyA5L2z6L+Q6L6TMDMwNGFiY2Q2NTAyMDA4MDM"
+        + "yMDAxNkEwMTEyMjMzNDQ5OTg4NzcwNzA4MTIzNDU2Nzg2MzA0NDIyMA=="));
+
     assertThat(new String(Base64.decodeBase64(merchantPresentMode.toBase64()), StandardCharsets.UTF_8), equalTo(merchantPresentMode.toString()));
 
   }
@@ -239,12 +261,25 @@ public class MerchantPresentedModeTest {
 
   @Test
   public void testSuccessToHex() throws DecoderException, PresentedModeException {
-    final String encoded = "00020101021102160004hoge0104abcd520441115303156540523.725502015603500570155802CN5914BEST TRANSPORT6007BEIJING6107123456762950105123450205678900305098760405543210505abcde0605fghij0705klmno0805pqres0905tuvxy5010000110101i64280002ZH0102北京0204最佳运输0304abcd65020080320016A01122334499887707081234567863044220";
+    final String encoded = "00020101021102160004hoge0104abcd520441115303156540523.72"
+        + "5502015603500570155802CN5914BEST TRANSPORT6007BEIJING61071234567629501051"
+        + "23450205678900305098760405543210505abcde0605fghij0705klmno0805pqres0905tu"
+        + "vxy5010000110101i64280002ZH0102北京0204最佳运输0304abcd65020080320016A01122"
+        + "334499887707081234567863044220";
 
     final MerchantPresentedMode merchantPresentMode = DecoderMpm.decode(encoded, MerchantPresentedMode.class);
 
-    assertThat(merchantPresentMode.toHex(), equalTo(
-        "3030303230313031303231313032313630303034686F676530313034616263643532303434313131353330333135363534303532332E373235353032303135363033353030353730313535383032434E3539313442455354205452414E53504F5254363030374245494A494E4736313037313233343536373632393530313035313233343530323035363738393030333035303938373630343035353433323130353035616263646530363035666768696A303730356B6C6D6E6F303830357071726573303930357475767879353031303030303131303130316936343238303030325A4830313032E58C97E4BAAC30323034E69C80E4BDB3E8BF90E8BE9330333034616263643635303230303830333230303136413031313232333334343939383837373037303831323334353637383633303434323230"));
+    assertThat(merchantPresentMode.toHex(), equalTo("3030303230313031303231313032313"
+        + "630303034686F676530313034616263643532303434313131353330333135363534303532"
+        + "332E373235353032303135363033353030353730313535383032434E35393134424553542"
+        + "05452414E53504F5254363030374245494A494E4736313037313233343536373632393530"
+        + "3130353132333435303230353637383930303330353039383736303430353534333231303"
+        + "53035616263646530363035666768696A303730356B6C6D6E6F3038303570717265733039"
+        + "30357475767879353031303030303131303130316936343238303030325A4830313032E58"
+        + "C97E4BAAC30323034E69C80E4BDB3E8BF90E8BE9330333034616263643635303230303830"
+        + "3332303031364130313132323333343439393838373730373038313233343536373836333"
+        + "03434323230"));
+
     assertThat(new String(Hex.decodeHex(merchantPresentMode.toHex()), StandardCharsets.UTF_8), equalTo(merchantPresentMode.toString()));
 
   }
@@ -299,3 +334,4 @@ public class MerchantPresentedModeTest {
   }
 
 }
+// @formatter:on
