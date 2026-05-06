a529710babf49000f99a26494289af17c34071d9
Marcos Tischer Vallim <tischer@gmail.com>
2026-05-05 22:48:40 -0300

    chore: add full documentation java-docs
    Signed-off-by: Marcos Tischer Vallim <tischer@gmail.com>

--- src/main/java/com/emv/qrcode/validators/mpm/MerchantPresentedModeValidator.java
+++ src/main/java/com/emv/qrcode/validators/mpm/MerchantPresentedModeValidator.java
@@ -55,19 +55,18 @@ import br.com.fluentvalidator.AbstractValidator;
  * <p>According to EMVCo specifications, an MPM QR code must contain specific
  * mandatory fields and may contain various optional fields with format constraints.</p>
  *
- * <p>Key validation rules include:
- *   <ul>
- *     <li>Payload Format Indicator must be "01"</li>
- *     <li>Point of Initiation Method must be "11" or "12" (if present)</li>
- *     <li>Merchant Category Code must be 4 digits</li>
- *     <li>Transaction Currency must be 3 digits (ISO 4217)</li>
- *     <li>Transaction Amount must be a valid number (if present)</li>
- *     <li>Tip or Convenience Indicator validation with conditional fields</li>
- *     <li>Country Code must be 2 digits (ISO 3166-1)</li>
- *     <li>Merchant Name and City are mandatory</li>
- *     <li>At least one Merchant Account Information template required</li>
- *   </ul>
- * </p>
+ * <p>Key validation rules include:</p>
+ * <ul>
+ *   <li>Payload Format Indicator must be "01"</li>
+ *   <li>Point of Initiation Method must be "11" or "12" (if present)</li>
+ *   <li>Merchant Category Code must be 4 digits</li>
+ *   <li>Transaction Currency must be 3 digits (ISO 4217)</li>
+ *   <li>Transaction Amount must be a valid number (if present)</li>
+ *   <li>Tip or Convenience Indicator validation with conditional fields</li>
+ *   <li>Country Code must be 2 digits (ISO 3166-1)</li>
+ *   <li>Merchant Name and City are mandatory</li>
+ *   <li>At least one Merchant Account Information template required</li>
+ * </ul>
  *
  * @see com.emv.qrcode.model.mpm.MerchantPresentedMode
  * @see com.emv.qrcode.validators.mpm.AdditionalDataFieldTemplateValidator
